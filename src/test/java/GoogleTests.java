0386bc1cf6c9beeb762af52bc9b1c2cc090f1087
Kinmarloy <kinmarloy@yadex.ru>
2021-04-23 08:14:31 +0300

    Added GoogleSearchTest

---
+++ src/test/java/GoogleTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTests {
+    @Test
+    public void searchGoogleTest(){
+        open("https://www.google.com/");
+        $x("//input[@name='q']").setValue("Selenide").pressEnter();
+        $x("//div[@id='search']").shouldHave(text("Selenide.org"));
+    }
+
+}
